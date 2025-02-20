2113a51a1fd884e68760a3d198df7bc15bc54717
jmendes2022 <jmendes2022@gmail.com>
2025-02-20 13:38:05 -0500

    worked on sign up form.

--- signup.js
+++ signup.js
@@ -1,8 +1,11 @@
 const firstName = document.getElementById("firstName");
 const lastName = document.getElementById("lastName");
-const email = document.getElementById("email");
-const age = document.getElementById("age");
+const email = document.getElementById("Email");
+const age = document.getElementById("Age");
 const TermsandConditions = document.getElementById("TermsandConditions");
+
+const codeConfirmationForm = document.getElementById("confirm-code-form");
+const codeInput = document.getElementById("confirm-code");
 
 function GetRandomCode(event) {
   return Math.floor(100000 + Math.random() * 900000);
@@ -19,10 +22,28 @@ function SaveToLocalStorage(data) {
   }
 }
 
-function ClearStorage() {
+function ClearForm() {
   firstName.value = "";
   lastName.value = "";
   email.value = "";
   age.value = "";
   TermsandConditions.checked = false;
+}
+
+// this code below is not working properly - code not full verified and needs to be double checked!!
+
+function VerifyCode(e) {
+  e.preventDefault();
+
+  codeConfirmationForm.addEventListener("submit", function () {
+    const user = JSON.parse(localStorage.getItem("user"));
+
+    if (user.code == codeInput) {
+      user = { ...user, verified: true };
+      localStorage.setItem("user", JSON.stringify(user));
+      alert("Successfully Verified!");
+    } else {
+      alert("Invalid code!");
+    }
+  });
 }
