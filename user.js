3170ff464ebfeada5b38ca7d9cef98ed8e6908db
jmendes2022 <jmendes2022@gmail.com>
2025-02-20 20:46:03 -0500

    adjusted some login settings

--- user.js
+++ user.js
@@ -1,22 +1,48 @@
+const loggedUser = document.getElementById("loggedUser");
+const signIn = document.getElementById("signIn");
+
 GetUserData();
+// LoadTestData();
+
+function LoadTestData() {
+  const user = {
+    firstName: "Jordan",
+    lastName: "Mendes",
+    age: "29",
+    code: "150333",
+    email: "Jordanm1996.jm@gmail.com",
+    verified: true,
+    loggedIn: true,
+    DateLastLoggedIn: new Date().getHours() - 1,
+  };
+
+  localStorage.setItem("user", JSON.stringify(user));
+}
 
 function GetUserData() {
   let user = JSON.parse(localStorage.getItem("user"));
 
-  if (!user.DateLastLoggedIn) {
-    user = { ...user, verified: false };
+  if (user && !user.DateLastLoggedIn) {
+    user = { ...user, loggedIn: false };
+    localStorage.setItem("user", JSON.stringify(user));
+  }
+
+  const now = new Date().getHours();
+  const DateLastLoggedIn = user.DateLastLoggedIn;
+  const timeDifference = now - DateLastLoggedIn;
+  const hoursElapsed = parseInt(timeDifference);
+
+  if (!hoursElapsed == 0 && user.loggedIn) {
+    user = { ...user, loggedIn: false };
+    localStorage.setItem("user", JSON.stringify(user));
+
     alert("you have been logged out due to inactivity. Please log in again!");
     window.location.href = "/login.html";
   }
 
-  const today = new Date();
-  const DateLastLoggedIn = user.DateLastLoggedIn;
-  const timeDifference = today - DateLastLoggedIn;
-  const hoursElapsed = timeDifference / (1000 * 60 * 60);
-
-  if (hoursElapsed >= 24) {
-    user = { ...user, verified: false };
-    alert("you have been logged out due to inactivity. Please log in again!");
-    window.location.href = "/login.html";
+  if (user && user.verified && user.loggedIn && hoursElapsed == 0) {
+    loggedUser.innerHTML = `Hi, ${user.firstName}!`;
+    loggedUser.classList.toggle("hidden");
+    signIn.classList.toggle("hidden");
   }
 }
